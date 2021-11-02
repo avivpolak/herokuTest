e759eeb02d89f2b05f2540af2a4ee8bea62793f2
[aviv polak] <[avivp0437@gmail.com]>
2021-11-02 06:27:50 +0200

    deployment

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3000;
-
+app.use(cors);
 app.use("/", (req, res) => {
     res.send("hello heroku test");
 });
